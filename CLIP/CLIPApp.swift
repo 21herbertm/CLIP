c2ffc069f1617df6f2ec4f13ba785d92c4183973
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-06-16 10:06:10 -0400

    Initial Commit

---
+++ CLIP/CLIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLIPApp.swift
+//  CLIP
+//
+//  Created by Melanie Herbert on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CLIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
