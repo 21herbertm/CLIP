5235a047444fdb1bcf4c87b9a1551ceced9b67f4
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-06-20 12:56:34 -0400

    BluetoothConnect_1
    -shows list of all bluetooth devices
    -button to scan and stop scanning for devices
    -allows user to connect to bluetooth device
    -once connected allows user to disconnect
    
    FOR FUTURE: filter specific titles/types of devices

--- CLIP/CLIPApp.swift
+++ CLIP/CLIPApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import iOSDFULibrary
 
 @main
 struct CLIPApp: App {
--- CLIP/ContentView.swift
+++ CLIP/ContentView.swift
@@ -6,21 +6,72 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @StateObject var bluetoothManager = BluetoothManager()
+    @State private var isScanning = false
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                Color(UIColor.systemMint)
+                    .frame(height: 120)
+                    .edgesIgnoringSafeArea(.top)
+
+                GeometryReader { geometry in
+                    VStack {
+                        if bluetoothManager.discoveredDevices.isEmpty {
+                            Spacer()
+
+                            Text("No devices found")
+                                .frame(maxWidth: .infinity)
+                                .font(.headline)
+                                .padding()
+                                .background(Color.white)
+                                .foregroundColor(.black)
+                                .cornerRadius(10)
+                                .padding()
+                                .shadow(radius: 4)
+
+                            Spacer()
+                        } else {
+                            List(Array(bluetoothManager.discoveredDevices.enumerated()), id: \.element.identifier) { (index, device) in
+                                NavigationLink(destination: DeviceDetailView(device: device)
+                                    .environmentObject(bluetoothManager)
+                                ) {
+                                    Text(device.name ?? "Unknown Device")
+                                }
+                            }
+                        }
+
+                        Button(action: {
+                            if isScanning {
+                                bluetoothManager.stopScanning()
+                            } else {
+                                bluetoothManager.startScanning()
+                            }
+                            isScanning.toggle()
+                        }) {
+                            Text(isScanning ? "Stop Scanning" : "Scan for Devices")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .padding()
+                                .shadow(radius: 4)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationTitle("")
+            .onAppear {
+                bluetoothManager.startScanning()
+            }
+        }
+        .environmentObject(bluetoothManager)
     }
 }
