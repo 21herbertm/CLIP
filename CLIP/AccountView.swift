04c3502185b9228c59b85d79ecce24717ac16d4a
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2023-07-31 10:46:27 -0400

    Update AccountView.swift

--- CLIP/AccountView.swift
+++ CLIP/AccountView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct AccountView: View {
+    @EnvironmentObject var sessionManager: SessionManager
+    let user: AuthUser
+    
     var body: some View {
         VStack {
             Text("Account Page")
+            
+            Text("You signed in as \(user.username) using Amplify!!!")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
             Button("Update Firmware") {
                 // Call your function to update firmware here
             }
@@ -18,6 +26,10 @@ struct AccountView: View {
             .background(Color.blue)
             .foregroundColor(.white)
             .cornerRadius(10)
+
+            Spacer()
+            
+            Button("Sign Out", action: sessionManager.signOut)
         }
         .navigationBarTitle("Account", displayMode: .inline)
     }
